da7b313006301ea7d5e070e6249aecf1b302322a
husneara <husnearaa99@gmail.com>
2023-12-20 15:50:00 +0600

    has updated banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,40 +4,38 @@ import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className=" h-screen w-full bg-[#081b29]">
-            <div className="h-[100vh] mx-auto flex flex-col items-center justify-between pt-10 px-28
-            md:flex-row text-white">
-                <div className="mt-26">
+        <div className="bg-[#081b29]">
+            <div className="flex items-center justify-between max-w-6xl h-[100vh] mx-auto py-20">
+                <div className="w-full text-center md:text-left md:w-7/12 text-white ">
                     <h2 className="md:text-5xl text-2xl font-extrabold">Hi, I'm Hosneara Popy</h2>
-                    <p className="md:text-3xl text-xl font-extrabold text-[#01a1e5] mt-3">Frontend Developer</p>
-                    <p className="mt-4">I work on web application using technology like html, tailwind CSS, <br /> React.js,
+                    <h2 className="md:text-3xl font-extrabold text-xl text-[#01a1e5] mt-3">Frontend Developer
+                    </h2>
+                    <p className="text-sm md:text-base font-medium text-white mt-3">
+                        I work on web application using technology like html, <br /> tailwind CSS, React.js,
                         express and MongoDB.
                     </p>
-                    <div className="flex gap-5">
-                        <div>
-                            <a href="/src/assets/Assignment-02.pdf" download="Resume">
-                                <button className="border-[#01a1e5] border-2 rounded-md py-2 px-3 text-[#081b29]
-                                  bg-[#01a1e5] hover:bg-[#081b29] hover:text-[#01a1e5] mt-5 md:mt-10">
-                                    Download Resume
-                                </button>
-                            </a>
-                        </div>
-                        <div>
-                            <Link to={"/contact"}>
-                                <button className=" border-[#01a1e5] border-2 rounded-md py-2 px-3 text-[#01a1e5]  hover:bg-[#01a1e5]
-                               hover:text-[#081b29] md:mt-10 mt-5">
-                                    Let's Talk
-                                </button>
-                            </Link>
-                        </div>
+                    <div className="flex gap-5 justify-center md:justify-normal mt-5">
+                        <a href="/src/assets/Assignment-02.pdf" download="Resume" >
+                            <button className="border-[#01a1e5] border-2 rounded-md py-2 px-3 text-[#081b29]
+                                  bg-[#01a1e5] hover:bg-[#081b29] hover:scale-105 hover:text-[#01a1e5]">
+                                Download Resume
+                            </button>
+                        </a>
+                        <Link to={"/contact"}>
+                            <button className=" border-[#01a1e5] border-2 rounded-md py-2 px-3 text-[#01a1e5]  hover:bg-[#01a1e5]
+                               hover:text-[#081b29] hover:scale-105 ">
+                                Let's Talk
+                            </button>
+                        </Link>
                     </div>
-                    <div className="flex gap-5 pt-8 ">
-                        <a href="https://github.com/husnearaa" target="_blank" rel="noreferrer"><FaGithub className="text-4xl text-[#01a1e5]"></FaGithub></a>
-                        <a href="https://www.linkedin.com/in/hosneara-popy-911abb136/" target="_blank" rel="noreferrer"><FaLinkedin className="text-4xl text-[#01a1e5]"></FaLinkedin></a>
+                    <div className="flex gap-5 md:pt-16 pt-5 justify-center md:justify-normal">
+                        <a href="https://github.com/husnearaa" target="_blank" rel="noreferrer">
+                            <FaGithub className="text-4xl text-[#01a1e5]"></FaGithub>
+                        </a>
+                        <a href="https://www.linkedin.com/in/hosneara-popy-911abb136/" target="_blank" rel="noreferrer">
+                            <FaLinkedin className="text-4xl text-[#01a1e5]"></FaLinkedin>
+                        </a>
                     </div>
-                </div>
-                <div className="">
-                    {/* <img className="mx-auto w-2/3 md:w-full" src={"https://i.postimg.cc/K8MncCDd/IMG-20200313-114257-fotor-bg-remover-2023121415510.png"} alt="" /> */}
                 </div>
             </div>
         </div>
